2cee8fa4e90d69937234e50449aa057273a32d90
Ilya <ilyavanavara@gmail.com>
2016-12-18 19:27:36 +0700

    base project

--- src/main/java/com/analitics/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.analitics.constant;
-
-public class Constant {
-    public static final String OUATH_TOKEN = "AQAAAAASjEe8AAOfZ4L88v3m-U9PvgBL9J0AI-g";
-    public static final String HOST = "https://api-metrika.yandex.ru/stat/v1/data?preset=source_summary&id=38437860&oauth_token=AQAAAAASjEe8AAOfZ4L88v3m-U9PvgBL9J0AI-g";
-    public static final String HOST2 = "https://api-metrika.yandex.ru/stat/v1/data?preset=sources_summary&id=38437860&oauth_token=AQAAAAASjEe8AAOfZ4L88v3m-U9PvgBL9J0AI-g";
-    public static final String VISIED ="https://api-metrika.yandex.ru/stat/v1/data/bytime?date1=2016-10-28&date2=2016-11-27&group=month&dimensions=ym:s:%3Cattribution%3ETrafficSource&attribution=last&ids=38437860&metrics=ym:s:visits&oauth_token=AQAAAAASjEe8AAOfZ4L88v3m-U9PvgBL9J0AI-g";
-
-}
